c4909751e42dba8a5757fa29d1da3b9183ad5874
BENZENE <jirayu.t@altplusx.com>
2022-12-20 15:38:00 +0700

    add echo http framework

--- server.go
+++ server.go
@@ -1,11 +1,27 @@
 package main
 
 import (
-	"fmt"
-	"os"
+	"log"
+	"net/http"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 func main() {
-	fmt.Println("Please use server.go for main file")
-	fmt.Println("start at port:", os.Getenv("PORT"))
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/health", func(c echo.Context) error {
+		return c.String(http.StatusOK, "OK")
+	})
+
+	// e.POST("/expenses", createUserHandler)
+	// e.GET("/expenses", getUsersHandler)
+
+	log.Fatal(e.Start(":2565"))
+
+	// fmt.Println("Please use server.go for main file")
+	// fmt.Println("start at port:", os.Getenv("PORT"))
 }
